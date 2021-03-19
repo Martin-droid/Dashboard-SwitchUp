61bc0912b6de1920d3f6a58a607f1316d7c9ae0e
Martin Ndegwa <51788472+Martin-droid@users.noreply.github.com>
2021-03-19 22:46:29 +0300

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>signup.switchup</title>
+	<link rel="stylesheet"  href="assets/dist/css/signup.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/ef7e8231ee.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="sign">
+	<div class="pic">
+		<img src="images/switch2.png">
+	<h2>Login Here</h2>
+	<form>
+		<input type="text" name="" placeholder="Email.."required>
+			<input type="password"placeholder="password.." required>
+
+		<input type="submit"value="Login">
+	</form>
+</div>
+</body>
+</html>
